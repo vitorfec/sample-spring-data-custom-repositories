c21023b03d4075b8862111a51a245699c5c76de0
vitor-home <vitorfec@hotmail.com>
2019-12-30 18:23:31 +0000

    Project setup

---
+++ src/main/java/org/springframework/data/samplespringdatacustomrepositories/SampleSpringDataCustomRepositoriesApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.data.samplespringdatacustomrepositories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringDataCustomRepositoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringDataCustomRepositoriesApplication.class, args);
+	}
+
+}
